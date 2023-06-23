4aaf418693160aa8f6fdbbb4b335b42537083a16
DudaJAK <134089925+DudaJAK@users.noreply.github.com>
2023-06-23 16:14:57 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,29 @@
+"""
+programa dicionario
+descricao: esse programa apresenta a estrutura de conjuntos em python
+autor: duda jak tlgd
+data: 23/06/2023
+versao: 0.0.1
+"""
+
+# atribuição de variáveis
+
+
+lista = [1,2,3,3,7,9,22,22,22,8,47,2]
+conjunto_2 = {1,3,3,5,5,0,0,23,-2}
+
+
+# entrada de dados
+conjunto_1 = set(lista)
+
+
+
+# processamento de dados
+conjunto_2.add(49)
+conjunto_2.remove(23)
+
+
+
+# saída de dados
+print(conjunto_1)
+print(conjunto_2)
